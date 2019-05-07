915441d64db696d9f1dda477b20fe393cb81fb21
Brad Cas <bmcasalvieri@gmail.com>
2019-05-06 21:16:41 -0400

    htmlRoutes.js set up, main.handlebars set up

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,22 @@
+// import our burgers model
+const burgers = require("../models/burger");
+
+// export our route definitions as a function
+module.exports = (app) => {
+
+  app.get("/", function(req, res) {
+
+    // use burgers.findAll
+    burgers
+      .findAll()
+      // if we get to resolve()
+      .then(dbBurgerData => {
+        res.render("index", {catData: dbBurgerData})
+      })
+      // if we get to reject()
+      .catch(err => {
+        console.log(err);
+        res.json(err);
+      });
+  });
+};
